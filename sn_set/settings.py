dc24e1ae44bce5b09e3010483024f2614f0999b7
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-05-06 13:35:57 -0400

    updated env variable names

--- sn_set/settings.py
+++ sn_set/settings.py
@@ -3,5 +3,5 @@ from environs import Env
 env = Env()
 env.read_env()
 
-USER = env.str("USER_NAME")
-PASSWORD = env.str("PASSWORD")
+USER = env.str("SN_USER_NAME")
+PASSWORD = env.str("SN_PASSWORD")
