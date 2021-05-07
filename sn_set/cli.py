5066770a6ea01bd60f1e00320dffbae7c04465f8
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-05-06 23:44:01 -0400

    added cli options and tests

--- sn_set/cli.py
+++ sn_set/cli.py
@@ -2,12 +2,18 @@ import click
 
 
 @click.command()
-def main():
+@click.option(
+    "--target", "-t", required=True, help="The instance you want to compare to"
+)
+@click.option(
+    "--source", "-s", required=True, help="The instance you want update sets from"
+)
+def main(source, target):
     """
     snset is a python cli tool for retrieving the list of installed
     update sets in two ServiceNow instances, comparing them, and
-    outputting the list of update sets that are installed in the target
-    instance and not installed in the source instance, in the order
+    outputting the list of update sets that are installed in the source
+    instance and not installed in the target instance, in the order
     that they must be installed in.
 
     Usage:
@@ -17,6 +23,6 @@ def main():
     Will output to an excel file in the current directory, unless another
     is specified.
     """
-    print("Entrypoint for the program")
+    print(f"Begin retrieving update sets from source: {source} and target: {target}")
 
     # TODO set up github actions CI integration
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,29 @@ from sn_set import cli
 
 def test_cli(runner):
     result = runner.invoke(cli.main)
+    assert result.exit_code != 0
+    assert result.exception
+
+
+def test_cli_no_source(runner):
+    result = runner.invoke(cli.main, ["--target", "nyudev"])
+    assert result.exit_code != 0
+    assert result.exception
+
+
+def test_cli_no_target(runner):
+    result = runner.invoke(cli.main, ["--source", "nyudev"])
+    assert result.exit_code != 0
+    assert result.exception
+
+
+def test_cli_valid(runner):
+    test_source = "nyudev"
+    test_target = "nyuqa"
+    result = runner.invoke(cli.main, ["--source", test_source, "--target", test_target])
     assert result.exit_code == 0
     assert not result.exception
-    assert "Entrypoint for the program" in result.output.strip()
+    assert (
+        f"Begin retrieving update sets from "
+        f"source: {test_source} and target: {test_target}"
+    ) in result.output
