d97c52bd34f1eafd18fb04ded8a46cf01a6a7ef4
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-05-08 10:24:13 -0400

    added tests for main method

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 from os import path
+from unittest import mock
 
 import pytest
 
@@ -23,34 +24,204 @@ def test_cli_no_target(runner):
     assert result.exception
 
 
-# def test_cli_valid(runner, monkeypatch):
-#     mock_sets = [
-#         {
-#             'name': 'a set',
-#             'sys_id': '12345'
-#         },
-#         {
-#             'name': 'b set',
-#             'sys_id': '54321'
-#         }
-#     ]
+@mock.patch("sn_set.cli.to_excel")
+@mock.patch("sn_set.cli.get_set_diff")
+@mock.patch("sn_set.cli.get_install_order_new")
+@mock.patch("sn_set.cli.get_install_order")
+@mock.patch("sn_set.cli.get_update_sets")
+def test_cli_valid(
+    mock_get_update_sets,
+    mock_get_install_order,
+    mock_new_install_order,
+    mock_set_diff,
+    mock_to_excel,
+    runner,
+):
+    mock_set1 = [
+        {"name": "a set", "sys_id": "12345"},
+        {"name": "b set", "sys_id": "54321"},
+    ]
+    mock_set2 = [{"name": "a set", "sys_id": "12345"}]
+    mock_install_order = [{"name": "a set", "sys_id": "12345"}]
 
-#     def mock_get_update_sets(instance_name):
-#         return mock_sets
+    mock_get_update_sets.side_effect = [mock_set1, mock_set2]
+    mock_get_install_order.return_value = mock_install_order
+    mock_set_diff.side_effect = [["a set"], []]
+    mock_to_excel.return_value = True
 
-#     monkeypatch.setattr(cli, "get_update_sets", mock_get_update_sets)
-#     monkeypatch.setattr(cli, "get_install_order", mock_get_update_sets)
+    test_source = "nyudev"
+    test_target = "nyuqa"
+    result = runner.invoke(cli.main, ["--source", test_source, "--target", test_target])
 
-#     test_source = "nyudev"
-#     test_target = "nyuqa"
-#     result = runner.invoke(cli.main,
-#       ["--source", test_source, "--target", test_target])
-#     assert result.exit_code == 0
-#     assert not result.exception
-#     assert (
-#         f"Begin retrieving update sets from "
-#         f"source: {test_source} and target: {test_target}"
-#     ) in result.output
+    mock_get_update_sets.assert_any_call("nyudev")
+    mock_get_update_sets.assert_any_call("nyuqa")
+    mock_set_diff.assert_any_call(["a set", "b set"], ["a set"])
+    mock_new_install_order.assert_not_called()
+    mock_to_excel.assert_called_once_with(mock_install_order, None)
+
+    assert result.exit_code == 0
+    assert not result.exception
+    assert (
+        f"Begin retrieving update sets from "
+        f"source: {test_source} and target: {test_target}"
+    ) in result.output
+    assert "Success" in result.output
+
+
+@mock.patch("sn_set.cli.to_excel")
+@mock.patch("sn_set.cli.get_set_diff")
+@mock.patch("sn_set.cli.get_install_order_new")
+@mock.patch("sn_set.cli.get_install_order")
+@mock.patch("sn_set.cli.get_update_sets")
+def test_cli_valid_with_new(
+    mock_get_update_sets,
+    mock_get_install_order,
+    mock_new_install_order,
+    mock_set_diff,
+    mock_to_excel,
+    runner,
+):
+    mock_set1 = [
+        {"name": "a set", "sys_id": "12345"},
+        {"name": "b set", "sys_id": "54321"},
+        {"name": "c set", "sys_id": "0987"},
+    ]
+    mock_set2 = [{"name": "a set", "sys_id": "12345"}]
+    mock_install_order = [{"name": "b set", "sys_id": "12345"}]
+    mock_final_install_order = [
+        {"name": "b set", "sys_id": "12345"},
+        {"name": "c set", "sys_id": "0987"},
+    ]
+
+    mock_get_update_sets.side_effect = [mock_set1, mock_set2]
+    mock_get_install_order.return_value = mock_install_order
+    mock_set_diff.side_effect = [["b set", "c set"], ["c set"]]
+    mock_new_install_order.return_value = [{"name": "c set", "sys_id": "0987"}]
+    mock_to_excel.return_value = True
+
+    test_source = "nyudev"
+    test_target = "nyuqa"
+    result = runner.invoke(cli.main, ["--source", test_source, "--target", test_target])
+
+    mock_get_update_sets.assert_any_call("nyudev")
+    mock_get_update_sets.assert_any_call("nyuqa")
+    mock_set_diff.assert_any_call(["a set", "b set", "c set"], ["a set"])
+    mock_set_diff.assert_any_call(["a set", "b set", "c set"], ["a set"])
+    mock_new_install_order.assert_called_with("nyudev", ["c set"])
+    mock_to_excel.assert_called_once_with(mock_final_install_order, None)
+
+    assert result.exit_code == 0
+    assert not result.exception
+    assert (
+        f"Begin retrieving update sets from "
+        f"source: {test_source} and target: {test_target}"
+    ) in result.output
+    assert "Success" in result.output
+
+
+@mock.patch("sn_set.cli.to_excel")
+@mock.patch("sn_set.cli.get_set_diff")
+@mock.patch("sn_set.cli.get_install_order_new")
+@mock.patch("sn_set.cli.get_install_order")
+@mock.patch("sn_set.cli.get_update_sets")
+def test_cli_valid_with_file(
+    mock_get_update_sets,
+    mock_get_install_order,
+    mock_new_install_order,
+    mock_set_diff,
+    mock_to_excel,
+    runner,
+):
+    mock_set1 = [
+        {"name": "a set", "sys_id": "12345"},
+        {"name": "b set", "sys_id": "54321"},
+        {"name": "c set", "sys_id": "0987"},
+    ]
+    mock_set2 = [{"name": "a set", "sys_id": "12345"}]
+    mock_install_order = [{"name": "b set", "sys_id": "12345"}]
+    mock_final_install_order = [
+        {"name": "b set", "sys_id": "12345"},
+        {"name": "c set", "sys_id": "0987"},
+    ]
+
+    mock_get_update_sets.side_effect = [mock_set1, mock_set2]
+    mock_get_install_order.return_value = mock_install_order
+    mock_set_diff.side_effect = [["b set", "c set"], ["c set"]]
+    mock_new_install_order.return_value = [{"name": "c set", "sys_id": "0987"}]
+    mock_to_excel.return_value = True
+
+    test_source = "nyudev"
+    test_target = "nyuqa"
+    test_file = "nyudev_order"
+    result = runner.invoke(
+        cli.main, ["--source", test_source, "--target", test_target, "-f", test_file]
+    )
+
+    mock_get_update_sets.assert_any_call("nyudev")
+    mock_get_update_sets.assert_any_call("nyuqa")
+    mock_set_diff.assert_any_call(["a set", "b set", "c set"], ["a set"])
+    mock_set_diff.assert_any_call(["a set", "b set", "c set"], ["a set"])
+    mock_new_install_order.assert_called_with("nyudev", ["c set"])
+    mock_to_excel.assert_called_once_with(mock_final_install_order, test_file)
+
+    assert result.exit_code == 0
+    assert not result.exception
+    assert (
+        f"Begin retrieving update sets from "
+        f"source: {test_source} and target: {test_target}"
+    ) in result.output
+    assert "Success" in result.output
+
+
+@mock.patch("sn_set.cli.to_excel")
+@mock.patch("sn_set.cli.get_set_diff")
+@mock.patch("sn_set.cli.get_install_order_new")
+@mock.patch("sn_set.cli.get_install_order")
+@mock.patch("sn_set.cli.get_update_sets")
+def test_cli_valid_with_failed_excel(
+    mock_get_update_sets,
+    mock_get_install_order,
+    mock_new_install_order,
+    mock_set_diff,
+    mock_to_excel,
+    runner,
+):
+    mock_set1 = [
+        {"name": "a set", "sys_id": "12345"},
+        {"name": "b set", "sys_id": "54321"},
+        {"name": "c set", "sys_id": "0987"},
+    ]
+    mock_set2 = [{"name": "a set", "sys_id": "12345"}]
+    mock_install_order = [{"name": "b set", "sys_id": "12345"}]
+    mock_final_install_order = [
+        {"name": "b set", "sys_id": "12345"},
+        {"name": "c set", "sys_id": "0987"},
+    ]
+
+    mock_get_update_sets.side_effect = [mock_set1, mock_set2]
+    mock_get_install_order.return_value = mock_install_order
+    mock_set_diff.side_effect = [["b set", "c set"], ["c set"]]
+    mock_new_install_order.return_value = [{"name": "c set", "sys_id": "0987"}]
+    mock_to_excel.return_value = False
+
+    test_source = "nyudev"
+    test_target = "nyuqa"
+    result = runner.invoke(cli.main, ["--source", test_source, "--target", test_target])
+
+    mock_get_update_sets.assert_any_call("nyudev")
+    mock_get_update_sets.assert_any_call("nyuqa")
+    mock_set_diff.assert_any_call(["a set", "b set", "c set"], ["a set"])
+    mock_set_diff.assert_any_call(["a set", "b set", "c set"], ["a set"])
+    mock_new_install_order.assert_called_with("nyudev", ["c set"])
+    mock_to_excel.assert_called_once_with(mock_final_install_order, None)
+
+    assert result.exit_code == -1
+    assert result.exception
+    assert (
+        f"Begin retrieving update sets from "
+        f"source: {test_source} and target: {test_target}"
+    ) in result.output
+    assert "There was an error writing the spreadsheet" in result.output
 
 
 @pytest.mark.parametrize(
@@ -82,6 +253,7 @@ def test_to_excel_valid(test_value, expected_value, runner):
     test_set_list = [
         {"name": "set1", "sys_id": "12345"},
         {"name": "set2", "sys_id": "54321"},
+        {"name": "set3", "sys_id": {"display_value": "a value"}},
     ]
     with runner.isolated_filesystem():
         cli.to_excel(test_set_list, test_value)
