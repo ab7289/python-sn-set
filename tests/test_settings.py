0e60d68dd8472953df6b83df4038a59edb85fe40
Alex Biehl <ab7289@nyu.edu>
2026-05-12 14:10:30 -0400

    added basic tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -11,3 +11,13 @@ class TestSettings:
         assert test_settings.get_client_id() == "client_id"
         assert test_settings.get_client_secret() == "super-secure"
         assert test_settings.get_grant_type() == "password"
+
+    def test_basic_settings(self, mock_env_vars):
+        test_settings = Settings()
+
+        assert test_settings.get_user() == "user"
+        assert test_settings.get_password() == "password"
+        assert not test_settings.get_use_oauth()
+        assert test_settings.get_client_id() is None
+        assert test_settings.get_client_secret() is None
+        assert test_settings.get_grant_type() is None
